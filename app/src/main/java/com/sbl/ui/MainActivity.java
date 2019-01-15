3b14a811fd4211fc0a24b767e22f0ba3d5f2834a
songbl <791243928@qq.com>
2019-01-15 15:07:27 +0800

    app:layout_scrollFlags="scroll|exitUntilCollapsed"属性对MD风格控件有效。坑自己了

--- app/src/main/java/com/sbl/ui/MainActivity.java
+++ app/src/main/java/com/sbl/ui/MainActivity.java
@@ -4,16 +4,23 @@ import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView ;
+    private TextView textView;
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.scrollactivity);
+        setContentView(R.layout.test_01);
 
         //去掉阴影状态栏
         if (Build.VERSION.SDK_INT >= 21) {
@@ -22,6 +29,19 @@ public class MainActivity extends AppCompatActivity {
             decorView.setSystemUiVisibility(option);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
+
+//初始化数据
+        recyclerView = findViewById(R.id.recyclerview);
+        List<String> datas = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            datas.add("item " + i);
+        }
+
+        //设置LayoutManager为LinearLayoutManager
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //设置Adapter
+        recyclerView.setAdapter(new GeneralAdapter(this, datas));
+
 
     }
 }
